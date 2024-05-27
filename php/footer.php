da0c430a1f8f13f64d9ba30d985a81dcff5dfc67
David-Schoots <davidschoots@live.nl>
2024-05-27 11:53:04 +0200

    header en footer aangemaakt met styling

--- php/footer.php
+++ php/footer.php
@@ -1,2 +1,12 @@
+<!-- Copyright -->
+<footer class="bg-white text-center text-lg-start fixed-bottom">
+    <!-- Copyright -->
+  <div class="text-center p-3">
+    © CBR - 2024
+  </div>
+  <!-- Copyright -->
+</footer>
+
+  <!-- Copyright -->
 </body>
 </html>
--- php/header.php
+++ php/header.php
@@ -5,26 +5,42 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>CBR - Theorie examen: Rijbewijs B</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <!-- ICONS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" />
   </head>
   <body>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 
     <!-- Main Navbar -->
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <!-- <nav class="navbar navbar-expand-lg bg-body-tertiary">
       <div class="container-fluid">
         <div class="text-center">
           <img src="./assets/logo/cbr-logo.png" class="rounded" alt="logo" height="50px">
         </div>
-<!--         <a class="navbar-brand" href="../assets/logo/cbr-logo.png">Test</a>
- -->        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
           <ul class="navbar-nav">
-            <li class="nav-item">
-              <h3>CBR - Theorie examen: Rijbewijs C</h3>
-            </li>
+              <h3 class="text-center">Theorie examen: Rijbewijs B</h3>
           </ul>
         </div>
       </div>
     </nav>
+
+ -->
+ <nav class="navbar navbar-expand-lg" style="background:#009ddf"> 
+  <div class="container-fluid d-flex justify-content-between align-items-center">
+    <img src="./assets/logo/cbr-logo.png" class="rounded" alt="logo" height="30px">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
+      <ul class="navbar-nav mb-2 mb-lg-0">
+        <li class="nav-item">
+          <h4 class="h4 text-center">Theorie examen: Rijbewijs B</h4>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
