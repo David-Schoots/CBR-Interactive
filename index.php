4884ab96ab8dd8edb80cc14e88594570dc305aa0
David-Schoots <davidschoots@live.nl>
2024-05-28 11:24:09 +0200

    form is beter gestyled

--- index.php
+++ index.php
@@ -4,19 +4,21 @@
 ?>
 
 <!-- Begin Form -->
-<form action="" method="post" name="inlog-kandidaat">
-  <div class="mb-3">
-    <label for="voornaam" class="form-label">Voornaam:</label>
-    <input type="username" class="form-control" id="voornaam" aria-describedby="voornaam">
-  </div>
-  <div class="mb-3">
-    <label for="achternaam" class="form-label">Achternaam:</label>
-    <input type="password" class="form-control" id="achternaam">
-  </div>
-  <div class="mb-3">
-    <label for="examennummer" class="form-label">Examen Nummer:</label>
-    <input type="number" class="form-control" id="examennummer">
-    <div id="examennummer" class="form-text">Het examen nummer heb je ontvangen bij het aanmelden!</div>
-  </div>
-  <button type="submit" class="btn btn-success">Begin Examen</button>
-</form><!-- einde Form -->
+<div class="login-form text-center">
+  <form class="p-3 border rounded" action="" method="post" name="inlog-kandidaat">
+    <div class="mb-3">
+      <label for="voornaam" class="form-label">Voornaam:</label>
+      <input type="text" class="form-control" id="voornaam" name="voornaam" aria-describedby="voornaam">
+    </div>
+    <div class="mb-3">
+      <label for="achternaam" class="form-label">Achternaam:</label>
+      <input type="password" class="form-control" id="achternaam" name="achternaam">
+    </div>
+    <div class="mb-3">
+      <label for="examennummer" class="form-label">Examen Nummer:</label>
+      <input type="number" class="form-control" id="examennummer" name="examennummer">
+      <div id="examennummer" class="form-text">Het examen nummer heb je ontvangen bij het aanmelden!</div>
+    </div>
+    <button type="submit" class="btn btn-success">Begin Examen</button>
+  </form><!-- einde Form -->
+</div>
